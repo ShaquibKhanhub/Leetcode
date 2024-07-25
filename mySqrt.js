9c8ed4d36a23b9382e19100572f51ce870daa9b9
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-07-25 22:17:15 +0530

    Sqrt(x)

--- mySqrt.js
+++ mySqrt.js
@@ -7,21 +7,21 @@
 // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
 
 let mySqrt = function (x) {
-  if (x === 0) return 0;
+    if (x === 0) return 0;
 
-  let lowIdx = 0,
-    highIdx = x;
-  let res;
+    let lowIdx = 0,
+        highIdx = x;
+    let res;
 
-  while (lowIdx <= highIdx) {
-    let mid = Math.floor((lowIdx + highIdx) / 2);
-    if (mid * mid <= x) {
-      res = mid;
-      lowIdx = mid + 1;
-    } else {
-      highIdx = mid - 1;
+    while (lowIdx <= highIdx) {
+        let mid = Math.floor((lowIdx + highIdx) / 2);
+        if (mid * mid <= x) {
+            res = mid;
+            lowIdx = mid + 1;
+        } else {
+            highIdx = mid - 1;
+        }
     }
-  }
 
-  return res;
+    return res;
 };

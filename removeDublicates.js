95c55fe159cb1166bfb6b97e0c93db53d9080aa2
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-07-25 22:18:11 +0530

    removeDuplicates

--- removeDublicates.js
+++ removeDublicates.js
@@ -1,14 +1,14 @@
 //similarly its look for my left neighbour is same or not
 let removeDuplicates = function (nums) {
-  if (nums.length == 0) return 0;
+    if (nums.length == 0) return 0;
 
-  let k = 0;
-  for (let i = 0; i < nums.length; ++i) {
-    if (nums[i] != nums[i - 1]) {
-      nums[k++] = nums[i];
+    let k = 0;
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k++] = nums[i];
+        }
     }
-  }
-  return k;
+    return k;
 };
 
 //its look for if my right neighbour is same or not

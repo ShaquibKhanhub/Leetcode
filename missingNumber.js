85e93a5e996209624c3e9dae1032e715fa44536b
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-07-25 22:16:27 +0530

    Missing Number

--- missingNumber.js
+++ missingNumber.js
@@ -3,13 +3,13 @@
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array
 
 let missingNumber = function (nums) {
-  let sum = 0;
-  let n = nums.length;
-  let naturalNum = (n * (n + 1)) / 2;
+    let sum = 0;
+    let n = nums.length;
+    let naturalNum = (n * (n + 1)) / 2;
 
-  for (let i = 0; i < n; i++) {
-    sum += nums[i];
-  }
-  let reqNum = naturalNum - sum;
-  return reqNum;
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+    }
+    let reqNum = naturalNum - sum;
+    return reqNum;
 };

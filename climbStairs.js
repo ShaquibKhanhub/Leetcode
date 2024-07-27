45574372fb4d0e6b963a357dd55b7957584d9f2f
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-07-27 21:51:33 +0530

    Merge Sorted Array

--- climbStairs.js
+++ climbStairs.js
@@ -1,11 +1,8 @@
 // 70. Climbing Stairs
 
-
 // You are climbing a staircase. It takes n steps to reach the top.
 
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
- 
 
 // Example 1:
 
@@ -22,3 +19,16 @@
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+
+let climbStairs = function (n) {
+  // if(n<=2)return n;
+  let dp = [n + 2, 0];
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  if (n <= 2) return dp[n];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
